fd0841aa664e4b43da74f2a26ecb25b83ec2dc11
Gabyoliver606 <gabyoliver@alu.ufc.br>
2022-07-23 20:54:11 -0300

    atualizou repositorio

--- Aquecimento/matriz.cpp
+++ Aquecimento/matriz.cpp
@@ -4,129 +4,153 @@
 
 using namespace std;
 
-struct Pos{
+struct Pos {
     int l, c;
-    Pos(int l, int c):
-        l(l), c(c){}
-    }
-
-    bool operator==(Pos &pos){
+    Pos(int l, int c) {
+        this->l = l;
+        this->c = c;
+    }  
+    bool operator==(Pos pos) {
         return l == pos.l && c == pos.c;
     }
-
-    bool operator!=(Pos &pos){
+    bool operator!=(Pos pos) {
         return l != pos.l || c != pos.c;
     }
-
-    string str() {
-        stringstream ss;
+    std::string str() {
+        std::stringstream ss;
         ss << "(" << l << ", " << c << ")";
         return ss.str();
     }
 };
-
-ostream& operator<<(ostream &os, Pos &pos){
-    os << pos.str();
-    return os;
+std::ostream &operator<<(std::ostream &os, Pos pos) {
+    return os << pos.str();
 }
 
 template <class T>
-class matrix {
-    int nl{0}, nc{0};
-    std::vector<std::vector<T>> data;
-
-public:
-    
-    matrix(int nl, int nc) : 
-        nl(nl), nc(nc), data(nl, std::vector<T>(nc)) {
-    }
-
-   
-    matrix(int nl, int nc, T def) : 
-        nl(nl), nc(nc), data(nl, std::vector<T>(nc, def)) {
-    }
-
- 
-    int nlines() {
-        return nl;
-    }
-
-
-    int ncols() {
-        return nc;
-    }
-
-  
-    T& at(Pos pos) {
-        return data[pos.l][pos.c];
-    }
-
-   
-    T& operator[](Pos pos) {
-        return at(pos);
-    }
-
-   
-    std::vector<T> &operator[](int l) {
-        return data[l];
-    }
-
-    
-    bool inside(Pos pos) {
-        return !(pos.l < 0 || pos.l >= nl || pos.c < 0 || pos.c >= nc);
-    }
-
-
-    bool is_value(Pos pos, char value) {
-        return inside(pos) && at[pos] == value;
-    }
-
-    
-    std::string str() {
+class Matriz {
+    private:
+        int nl, nc;
+        vector<vector<T>> matriz;
+    public:
+        Matriz(int nl, int nc) {
+            this->nl = nl;
+            this->nc = nc;
+            matriz.resize(nl);
+            for (int i = 0; i < nl; i++) {
+                matriz[i].resize(nc);
+            }
+        }
+        Matriz(vector<vector<T>> matriz) {
+            this->matriz = matriz;
+            this->nl = matriz.size();
+            this->nc = matriz[0].size();
+        }
+        Matriz(Matriz<T> matriz) {
+            this->matriz = matriz.matriz;
+            this->nl = matriz.nl;
+            this->nc = matriz.nc;
+        }
+        int getNl() {
+            return nl;
+        }
+        int getNc() {
+            return nc;
+        }
+        T& operator()(int l, int c) {
+            return matriz[l][c];
+        }
+        vector<T> operator[](int l) {
+            return matriz[l];
+        }
+        bool inside(Pos pos) {
+            return pos.l >= 0 && pos.l < nl && pos.c >= 0 && pos.c < nc;
+        }
+        bool is_value(Pos pos, T valor) {
+            return matriz[pos.l][pos.c] == valor;
+        }
+        string str() {
+            std::stringstream ss;
+            for (int i = 0; i < nl; i++) {
+                for (int j = 0; j < nc; j++) {
+                    ss << matriz[i][j] << " ";
+                }
+                ss << endl;
+            }
+            return ss.str();
+        }
+};
+template <>
+    string Matriz<char>::str() {
         std::stringstream ss;
-        for (int l = 0; l < nl; l++) {
-            for (int c = 0; c < nc; c++)
-                ss << (c == 0 ? "" : " ") << data[l][c];
-            ss << '\n';
+        for (int i = 0; i < nl; i++) {
+            for (int j = 0; j < nc; j++) {
+                ss << matriz[i][j] << " ";
+            }
+            ss << endl;
         }
         return ss.str();
     }
-};
-
-template <>
-std::string matrix<char>::str() {
-
-    std::stringstream ss;
-    for (int l = 0; l < nl; l++) {
-        for (int c = 0; c < nc; c++)
-            ss << data[l][c]; 
-        ss << '\n';
-    }
-    return ss.str();
-}
-
 template <class T>
-std::ostream& operator<<(std::ostream& os, matrix<T>& m) {
-    return os << m.str();
+    std::ostream &operator<<(std::ostream &os, Matriz<T> matriz) {
+        return os << matriz.str();
 }
 
-int main() {
-    {
-        matrix<int> m(3, 4, 5);
-        std::cout << m;
+int main () {
+    int nl, nc;
+    cin >> nl >> nc;
+    Matriz<int> matriz(nl, nc);
+    for (int i = 0; i < nl; i++) {
+        for (int j = 0; j < nc; j++) {
+            cin >> matriz(i, j);
+        }
     }
-    {
-        matrix<char> m(3, 4, 'a');
-        std::cout << m;
+    Matriz<int> rmatriz(nl, nc);
+    for (int i = 0; i < nl; i++) {
+        for (int j = 0; j < nc; j++) {
+            rmatriz(i, j) = matriz(nl - i - 1, nc - j - 1);
+        }
     }
-    {
-        matrix<int> m(3, 4);
-        m[0][0] = 1; 
-        m[{2, 2}] = 2; 
-        std::cout << m;
-        std::cout << m.inside({-1, 2}) << '\n';
-        std::cout << m.is_value({2, 2}, 2) << '\n';
-        std::cout << m.is_value({-1, 2}, 0) << '\n';
+    int sum = 0;
+    for (int i = 0; i < nl; i++) {
+        for (int j = 0; j < nc; j++) {
+            sum += rmatriz(i, j);
+        }
     }
-
+    int mult = 1;
+    for (int i = 0; i < nl; i++) {
+        for (int j = 0; j < nc; j++) {
+            mult *= rmatriz(i, j);
+        }
+    }
+    int max = rmatriz(0, 0);
+    for (int i = 0; i < nl; i++) {
+        for (int j = 0; j < nc; j++) {
+            if (rmatriz(i, j) > max) {
+                max = rmatriz(i, j);
+            }
+        }
+    }
+    int min = rmatriz(0, 0);
+    for (int i = 0; i < nl; i++) {
+        for (int j = 0; j < nc; j++) {
+            if (rmatriz(i, j) < min) {
+                min = rmatriz(i, j);
+            }
+        }
+    }
+    stringstream ss;
+    for(int i = 0; i < nl; i++) {
+        for(int j = 0; j < nc; j++) {
+            ss << rmatriz(i, j) << " ";
+        }
+        ss << endl;
+    }
+    cout << ss.str() << endl;
+    cout << sum << endl;
+    cout << mult << endl;
+    cout << max << endl;
+    cout << min << endl;
+    return 0;
 }
+
+
